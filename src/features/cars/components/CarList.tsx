a6b6582343f3d3606122fdf4681247953a917ef7
Yatin Burhmi <yatinburhmi@gmail.com>
2025-08-04 19:12:00 +0000

    feat: add CarList component and consume useCars hook

--- src/features/cars/components/CarList.tsx
+++ src/features/cars/components/CarList.tsx
@@ -1,3 +1,20 @@
-const CarList = () => {};
+import CardGrid from "@/components/ui/CardGrid/CardGrid";
+import useCars from "../hooks/useCars";
+import { Car } from "../car.types";
+import { CarCard } from "./CarCard";
+
+const CarList = () => {
+  const { cars, loading, error } = useCars();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <CardGrid>
+      {cars.map((car: Car) => (
+        <CarCard key={car.id} car={car} />
+      ))}
+    </CardGrid>
+  );
+};
 
 export default CarList;
